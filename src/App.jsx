0c70cb8cde5ecedd54942aa98223fb050923b081
Kartikay Gandhi <kartikaygandhi123@gmail.com>
2022-04-12 17:35:44 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to React Vite Micro App!</h1>
+      <p>
+        Hard to get more <strong>minimal</strong> than this React app.
+      </p>
+    </>
+  );
+};
+
+export default App;
