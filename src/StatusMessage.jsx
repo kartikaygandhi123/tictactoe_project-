f90a2f582474c0bd1b22d5c062f566fd0ce24bd2
Kartikay Gandhi <kartikaygandhi123@gmail.com>
2022-04-21 14:10:57 +0530

    styling is done

--- src/StatusMessage.jsx
+++ src/StatusMessage.jsx
@@ -4,13 +4,32 @@ function StatusMessage({ winner, current }) {
   const noMovesLeft = current.board.every(el => el !== null);
 
   return (
-    <h2>
-      {winner && `Winner is ${winner}`}
-      {!winner &&
-        !noMovesLeft &&
-        `Next player is ${current.isNext ? 'X' : 'O'}`}
-      {!winner && noMovesLeft && 'X and O tied'}
-    </h2>
+    <div className="status-message">
+      {winner && (
+        <>
+          Winner is{' '}
+          <span className={winner === 'X' ? 'text-green' : 'text-orange'}>
+            {winner}
+          </span>
+        </>
+      )}
+      {!winner && !noMovesLeft && (
+        <>
+          Next player is {''}
+          <span className={current.isNext ? 'text-green' : 'text-orange'}>
+            {current.isNext ? 'X' : 'O'}
+            {''}
+          </span>
+        </>
+      )}
+
+      {!winner && noMovesLeft && (
+        <>
+          <span className="text-green">X </span> and{' '}
+          <span className="text-orange">O</span>
+        </>
+      )}
+    </div>
   );
 }
 
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -5,8 +5,10 @@ function Square({ value, onClick, isWinningSquare }) {
   return (
     <button
       type="button"
-      className="square"
       onClick={onClick}
+      className={`square ${isWinningSquare ? 'winning' : ''} ${
+        value === 'X' ? 'text-green' : 'text-orange'
+      }`}
       style={{ fontWeight: isWinningSquare ? 'bold' : 'normal' }}
     >
       {value}
